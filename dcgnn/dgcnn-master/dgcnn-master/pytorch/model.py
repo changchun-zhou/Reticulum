4ee607d819982fea6de3cc277af7947ec7200b38
continuity3 <97281542+continuity3@users.noreply.github.com>
2024-12-26 01:37:40 +0000

    Update model.py

--- dcgnn/dgcnn-master/dgcnn-master/pytorch/model.py
+++ dcgnn/dgcnn-master/dgcnn-master/pytorch/model.py
@@ -152,23 +152,23 @@ class DGCNN(nn.Module):
 
         # Layer 1
         features = x
-        x = get_graph_feature_delayed_chunked(x, features, k=self.k, chunk_size=1024)
         x = self.conv1(x)
+        x = get_graph_feature_delayed_chunked(x, features, k=self.k, chunk_size=1024)
         x1 = x.max(dim=-1, keepdim=False)[0]
 
         # Layer 2
-        x = get_graph_feature_delayed_chunked(x, x, k=self.k, chunk_size=1024)
-        x = self.conv2(x)
+        x1 = self.conv2(x1)
+        x = get_graph_feature_delayed_chunked(x1, x1, k=self.k, chunk_size=1024)
         x2 = x.max(dim=-1, keepdim=False)[0]
 
         # Layer 3
-        x = get_graph_feature_delayed_chunked(x, x, k=self.k, chunk_size=1024)
-        x = self.conv3(x)
+        x2 = self.conv3(x2)
+        x = get_graph_feature_delayed_chunked(x2, x2, k=self.k, chunk_size=1024)
         x3 = x.max(dim=-1, keepdim=False)[0]
 
         # Layer 4
-        x = get_graph_feature_delayed_chunked(x, x, k=self.k, chunk_size=1024)
-        x = self.conv4(x)
+        x3 = self.conv4(x3)
+        x = get_graph_feature_delayed_chunked(x3, x3, k=self.k, chunk_size=1024)
         x4 = x.max(dim=-1, keepdim=False)[0]
         
         #维度的事情后面再说，维度可以根据excel当中的输入输出轻易改变，但是延迟聚合与原模型的不同之处在于特征的聚合不参与后续卷积
